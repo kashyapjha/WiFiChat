242a54623a0647db0539b5eb76ef45f3d0e83e6f
Kashyap Jha <kashyapjha1994@gmail.com>
2016-03-07 01:17:25 +0530

    Chat is up and running

--- app/src/main/java/com/kashyapjha/wifichat/ChatActivity.java
+++ app/src/main/java/com/kashyapjha/wifichat/ChatActivity.java
@@ -1,27 +1,144 @@
 package com.kashyapjha.wifichat;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ChatActivity extends AppCompatActivity
 {
-    String IP;
+    ArrayList<String> messagesList;
+    ArrayAdapter<String> adapter;
+    String messages;
+    int serverSocketPort=2048;
+    String chatIP;
+    ListView lvChat;
+    EditText etChatbox;
+    Button btnSend;
+    String TAG="WiFiChat";
+    String ownIP;
+    String path;
+
+    public void log(String msg)
+    {
+        Log.d(TAG,msg);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        messagesList=new ArrayList<String>();
+
+        adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,messagesList);
+
+        lvChat=(ListView)findViewById(R.id.lvChat);
+        lvChat.setDividerHeight(0);
+        lvChat.setDivider(null);
+        lvChat.setAdapter(adapter);
+
         Intent i=getIntent();
-        IP=i.getStringExtra("IP");
-        Log.d("WiFiChat",IP);
+        chatIP =i.getStringExtra("IP").substring(1);
+
+        path=getApplicationContext().getDir("chats",MODE_PRIVATE).getPath()+"/"+chatIP;
+
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        ownIP = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+
+        log("onCreate: Chat with host "+chatIP);
+
+        etChatbox=(EditText)findViewById(R.id.etChatbox);
+
+        btnSend=(Button)findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                log("btnSend.onClick: Send button clicked");
+                String message = etChatbox.getText().toString();
+                etChatbox.setText("");
+                //String senderIP = ownIP;
+                //String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+                JSONObject msgObject = new JSONObject();
+                try
+                {
+                    msgObject.put("Message", message);
+                    //msgObject.put("Sender IP", senderIP);
+                    //msgObject.put("Timestamp", timeStamp);
+                    log("btnSend.onClick: Message object created");
+                }
+                catch (JSONException e)
+                {
+                    log("btnSend.onClick: " + e.toString());
+                    log("btnSend.onClick: Message object not created");
+                }
+
+                File file = new File(path);
+
+                try
+                {
+                    if(!file.exists())
+                    {
+                        file.createNewFile();
+                        FileWriter fw=new FileWriter(file);
+                        fw.append("Me: "+msgObject.getString("Message"));
+                        fw.append("\n");
+                        fw.flush();
+                        fw.close();
+                    }
+                    else
+                    {
+                        FileWriter fw=new FileWriter(file,true);
+                        fw.append("Me: "+msgObject.getString("Message"));
+                        fw.append("\n");
+                        fw.flush();
+                        fw.close();
+                    }
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+                sendMsg(msgObject);
+                readMessages();
+                lvChat.setSelection(adapter.getCount()-1);
+            }
+        });
+
+        readMessages();
     }
 
     @Override
@@ -40,5 +157,114 @@ public class ChatActivity extends AppCompatActivity
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMsg(JSONObject msgObject)
+    {
+        log("sendMsg: Start sendMsg");
+        new SocketServerTask().execute(msgObject);
+        log("sendMsg: End sendMsg");
+    }
+
+    public void readMessages()
+    {
+        File file = new File(path);
+        if (file.exists())
+        {
+            try
+            {
+                Scanner fr=new Scanner(file);
+                fr.useDelimiter("\\Z"); // \Z means EOF.
+                messages = fr.next();
+                fr.close();
+            }
+            catch (FileNotFoundException e)
+            {
+                log(e.toString());
+            }
+        }
+        else
+        {
+            messages="No chats to display\n";
+        }
+        String[] messagesArray=messages.split("\n");
+
+        int count;
+
+        for(count=0;count<messagesArray.length;count++)
+        {
+            messagesList.add(messagesArray[count]);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    private class SocketServerTask extends AsyncTask<JSONObject, Void, Void>
+    {
+        private JSONObject jsonData;
+        private boolean success;
+
+        @Override
+        protected Void doInBackground(JSONObject... params)
+        {
+            log("SocketServerTask.doInBackground: doInBackground started");
+            DataOutputStream dataOutputStream = null;
+            Socket socket = null;
+            jsonData = params[0];
+            try
+            {
+                log("SocketServerTask.doInBackground: Creating socket");
+                socket = new Socket(chatIP, serverSocketPort);
+                log("SocketServerTask.doInBackground: Created socket");
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataOutputStream.writeUTF(jsonData.toString());
+                dataOutputStream.flush();
+                dataOutputStream.close();
+                socket.close();
+                log("SocketServerTask.doInBackground: Sending data " + jsonData.toString());
+                success=true;
+            }
+            catch (IOException e)
+            {
+                log("SocketServerTask.doInBackground: "+e.toString());
+                success = false;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            if(success)
+                log("onPostExecute: Message "+jsonData.toString()+" sent");
+            else log("onPostExecute: Message not sent");
+        }
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+
+        // Register mMessageReceiver to receive messages.
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("my-event"));
+    }
+
+    // handler for received Intents for the "my-event" event
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Extract data included in the Intent
+            String message = intent.getStringExtra("message");
+            Log.d("receiver", "Got message: " + message);
+            readMessages();
+            lvChat.setSelection(adapter.getCount()-1);
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        // Unregister since the activity is not visible
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        super.onPause();
     }
 }
--- app/src/main/java/com/kashyapjha/wifichat/ChatServerService.java
+++ app/src/main/java/com/kashyapjha/wifichat/ChatServerService.java
@@ -1,13 +1,30 @@
 package com.kashyapjha.wifichat;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ChatServerService extends Service
 {
+    static final int socketServerPort = 2048;
     String TAG="WiFiChat_ChatServerService";
     @Nullable
     @Override
@@ -17,6 +34,134 @@ public class ChatServerService extends Service
     }
     public void onCreate()
     {
-        Log.d(TAG,"Service Started");
+        Log.d(TAG, "onCreate: Service Started");
+        WifiManager wm = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        WifiManager.WifiLock wifiLock = wm.createWifiLock(WifiManager.WIFI_MODE_FULL, "WiFiChat_Lock");
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WiFiChat_Lock");
+
+        wifiLock.acquire();
+        wakeLock.acquire();
+
+        Log.d(TAG, "onCreate: Acquired Wakelocks");
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent,int flags, int startId)
+    {
+        //final Thread socketServerThread=new Thread(new SocketServerThread());
+        Log.d(TAG, "onStartCommand: Starting socketServerThread");
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                new SocketServerThread().run();
+            }
+        }).start();
+        return Service.START_STICKY;
+    }
+
+    private class SocketServerThread extends Thread
+    {
+        @Override
+        public void run()
+        {
+            Log.d(TAG,"SocketServerThread.run: socketServerThread running");
+
+            try
+            {
+                ServerSocket serverSocket=null;
+                DataInputStream dataInputStream=null;
+
+                while (true)
+                {
+                    serverSocket = new ServerSocket(socketServerPort);
+
+                    Log.d(TAG,"SocketServerThread.run: Server started on port "+serverSocket.getLocalPort());
+
+                    Socket socket = serverSocket.accept();
+
+                    Log.d(TAG, "SocketServerThread.run: Message received");
+
+                    dataInputStream = new DataInputStream(socket.getInputStream());
+                    String messageFromClient=dataInputStream.readUTF();
+
+                    final JSONObject msgObject=new JSONObject(messageFromClient);
+
+                    String path=getApplicationContext().getDir("chats",MODE_PRIVATE).getPath()+socket.getInetAddress();
+
+                    Log.d(TAG, path);
+
+                    File file = new File(path);
+                    if(!file.exists())
+                    {
+                        file.createNewFile();
+                        FileWriter fw=new FileWriter(file);
+                        fw.append("Them: "+msgObject.getString("Message"));
+                        fw.append("\n");
+                        fw.flush();
+                        fw.close();
+                        // write code for saving data to the file
+                    }
+                    else
+                    {
+                        FileWriter fw=new FileWriter(file,true);
+                        fw.append("Them: "+msgObject.getString("Message"));
+                        fw.append("\n");
+                        fw.flush();
+                        fw.close();
+                    }
+
+                    Log.d(TAG, "SocketServerThread.run: Message " + messageFromClient + " received from " + socket.getInetAddress() + ":" + socket.getPort());
+
+                    sendMessage();
+
+                    Handler h = new Handler(getMainLooper());
+
+                    h.post(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            try
+                            {
+                                Toast.makeText(getApplicationContext(), "Message received: "+msgObject.getString("Message"), Toast.LENGTH_SHORT).show();
+                            }
+                            catch (JSONException e)
+                            {
+                                Log.d(TAG, "SocketServerThread.run: "+e.toString());
+                            }
+                        }
+                    });
+
+                    Log.d(TAG, "SocketServerThread.run: Closing socket");
+                    socket.close();
+                    Log.d(TAG, "SocketServerThread.run: Closed socket");
+
+                    Log.d(TAG, "SocketServerThread.run: Closing serverSocket");
+                    serverSocket.close();
+                    Log.d(TAG, "SocketServerThread.run: serverSocket closed");
+                }
+            }
+
+            catch (IOException e)
+            {
+                Log.d(TAG, "SocketServerThread.run: "+e.toString());
+            }
+
+            catch (JSONException e)
+            {
+                Log.d(TAG, "SocketServerThread.run: "+e.toString());
+            }
+        }
+    }
+    private void sendMessage()
+    {
+        Intent intent = new Intent("my-event");
+        intent.putExtra("message", "data");
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 }
